82b854290062e0cb86eaeacd589a391b418ffa82
Alex Besogonov <cyberax@amazon.com>
2018-05-29 09:50:54 -0700

    Add listener interface

--- zinterface.go
+++ zinterface.go
@@ -3,6 +3,7 @@ package openzwave
 import (
 	"time"
 	"fmt"
+	"sync"
 )
 
 type ZwaveConfig struct {
@@ -15,15 +16,47 @@ type ZwaveConfig struct {
 	InitTimeout  time.Duration
 }
 
+type Listener chan <- Notification
+
+type NodeAddress struct {
+	HomeId uint32
+	NodeId uint8
+}
+
 type ZwaveInterface struct {
 	Manager Manager
 	HomeIdList map[uint32]string
+
+	listenerMutex sync.Mutex
+	globalListeners map[Listener] Listener
+	notifyDispatcher CallbackAdapter
+}
+
+func (zi *ZwaveInterface) AddListener(listener Listener) (){
+	zi.listenerMutex.Lock()
+	defer zi.listenerMutex.Unlock()
+
+	zi.globalListeners[listener] = listener
+}
+
+func (zi *ZwaveInterface) RemoveListener(listener Listener) (){
+	zi.listenerMutex.Lock()
+	defer zi.listenerMutex.Unlock()
+
+	delete(zi.globalListeners, listener)
+}
+
+func ensure(path string) string {
+	if path != "" && path[len(path)-1] != '/' {
+		return path + "/"
+	}
+	return path
 }
 
 func MakeZwaveInterface(options *ZwaveConfig) (*ZwaveInterface, error) {
 	var erroring_out = true
 
-	config := OptionsCreate(options.ZwaveLibDir, options.StateDir, "")
+	config := OptionsCreate(ensure(options.ZwaveLibDir), ensure(options.StateDir), "")
 	defer func() {
 		if erroring_out {
 			OptionsDestroy()
@@ -31,8 +64,8 @@ func MakeZwaveInterface(options *ZwaveConfig) (*ZwaveInterface, error) {
 	}()
 
 	config.AddOptionBool("SaveConfiguration", options.StateDir != "")
-	config.AddOptionString("UserPath", options.StateDir, false)
-	config.AddOptionString("ConfigPath", options.StateDir, false)
+	config.AddOptionString("UserPath", ensure(options.StateDir), false)
+	config.AddOptionString("ConfigPath", ensure(options.StateDir), false)
 
 	config.AddOptionBool("ConsoleOutput", options.LogToConsole)
 	config.AddOptionString("LogFileName", options.LogFile, false)
@@ -69,6 +102,7 @@ func MakeZwaveInterface(options *ZwaveConfig) (*ZwaveInterface, error) {
 	zi := new(ZwaveInterface)
 	zi.Manager = manager
 	zi.HomeIdList = make(map[uint32]string)
+	zi.globalListeners = make(map[Listener]Listener)
 
 	timeout := time.After(options.InitTimeout)
 	for len(zi.HomeIdList) < len(options.Adapters) {
@@ -82,14 +116,26 @@ func MakeZwaveInterface(options *ZwaveConfig) (*ZwaveInterface, error) {
 		}
 	}
 
+	zi.notifyDispatcher = MakeDelegatingHandler(func(notify Notification) {
+		zi.listenerMutex.Lock()
+		defer zi.listenerMutex.Unlock()
+		for k, _ := range zi.globalListeners {
+			k <- notify
+		}
+	})
+	AddNotifyHandler(manager, zi.notifyDispatcher)
+
 	erroring_out = false
 	return zi, nil
 }
 
 func (zwaveInterface *ZwaveInterface) StopZwaveInterface() {
+	RemoveNotifyHandler(zwaveInterface.Manager, zwaveInterface.notifyDispatcher)
+	DeleteCallbackAdapter(zwaveInterface.notifyDispatcher)
+
 	zwaveInterface.Manager = nil
 	zwaveInterface.HomeIdList = make(map[uint32]string)
+
 	ManagerDestroy()
 	OptionsDestroy()
 }
-
